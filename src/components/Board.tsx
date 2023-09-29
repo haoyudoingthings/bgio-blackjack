24f5db1ed03121a4bee038b3c176683e042d41f0
haoyudoingthings <allen.0327@gmail.com>
2023-09-29 23:07:58 +0800

    created initial game client

---
+++ src/components/Board.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface BoardProps {
+}
+
+const Board: React.FC<BoardProps> = () => {
+
+  return (
+    <h1>
+        Blackjack
+    </h1>
+  );
+}
+
+export default Board;
